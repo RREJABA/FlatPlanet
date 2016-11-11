7878da6268bfceb99565dacee880c592262f3e83
rrejaba <rrejaba@NPHIL201607-02>
2016-11-11 13:44:48 +1100

    Add project files.

---
+++ ClickCounter/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ClickCounter.DAL;
+using System.Web.Mvc;
+
+namespace ClickCounter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((object)DataAccess.GetCounter());
+        }
+
+        [HttpPost]
+        public ActionResult UpdateClickCount()
+        {
+            string msg = "Click count limit reached";
+
+            if (DataAccess.GetCounter() < 10)
+            {
+                DataAccess.UpdateCounter();
+                return RedirectToAction("Index", "Home");
+            }
+                
+
+            return View("Index", (object)msg);
+        }
+    }
+}
---
+++ ClickCounter/DAL/DataAccess.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClickCounter.DAL
+{
+    public static class DataAccess
+    {
+        public static readonly string connectionString = ConfigurationManager.ConnectionStrings["FlatPlanet"].ConnectionString;
+
+        public static int GetCounter()
+        {
+            SqlConnection sqlConn = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConn;
+            sqlCommand.CommandText = "SELECT Counter FROM ClickCounter WHERE ID = 1";
+            sqlConn.Open();
+
+            var reader = sqlCommand.ExecuteReader(CommandBehavior.SingleResult);
+            reader.Read();
+
+            var counter = (int)reader[0];
+
+            reader.Close();
+            sqlConn.Close();
+
+            return counter;
+        }
+
+        public static void UpdateCounter()
+        {
+            SqlConnection sqlConn = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConn;
+            sqlCommand.CommandText = "UPDATE ClickCounter SET Counter = Counter + 1 WHERE ID = 1";
+            sqlConn.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConn.Close();
+        }
+    }
+}
